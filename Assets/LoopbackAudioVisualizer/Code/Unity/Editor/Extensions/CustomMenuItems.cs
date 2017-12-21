4756684667aa83ef1e64c287fb7bd35a39ab2b77
Alessandro Attard Barbini <11708634+aleab@users.noreply.github.com>
2017-12-21 20:02:27 +0100

    Revert 89aa88; use custom Play function

---
+++ Assets/LoopbackAudioVisualizer/Code/Unity/Editor/Extensions/CustomMenuItems.cs
@@ -0,0 +1,30 @@
+﻿#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace Aleab.LoopbackAudioVisualizer.Unity.UnityEditor.Extensions
+{
+    [InitializeOnLoad]
+    public static class CustomMenuItems
+    {
+        static CustomMenuItems()
+        {
+        }
+
+        [MenuItem("Edit/Play-Pause from Scene 0 _F5", false, 10000)]
+        public static void PlayFromPrelaunchScene()
+        {
+            if (EditorApplication.isPlaying)
+                EditorApplication.isPlaying = false;
+            else
+            {
+                EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+                EditorSceneManager.OpenScene("Assets/LoopbackAudioVisualizer/00_Common.unity");
+                EditorApplication.isPlaying = true;
+            }
+        }
+    }
+}
+
+#endif
