214a2ce7af8b0153e550302b0f8c82f7bfa0ffb1
Alessandro Attard Barbini <11708634+aleab@users.noreply.github.com>
2018-01-07 17:03:34 +0100

    Add EditorCamera component

---
+++ Assets/LoopbackAudioVisualizer/Code/Scripts/EditorCamera.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+
+using UnityEditor;
+
+#endif
+
+namespace Aleab.LoopbackAudioVisualizer.Scripts
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(Camera))]
+    public class EditorCamera : MonoBehaviour
+    {
+        private Camera _camera;
+
+        [SerializeField]
+        private new bool enabled = true;
+
+        private void Awake()
+        {
+            this._camera = this.GetComponent<Camera>();
+        }
+
+#if UNITY_EDITOR
+
+        private void OnRenderObject()
+        {
+            if (!EditorApplication.isPlayingOrWillChangePlaymode)
+            {
+                if (this.enabled)
+                {
+                    var editorCamera = Camera.current;
+                    if (editorCamera != null)
+                    {
+                        this._camera.transform.position = editorCamera.transform.position;
+                        this._camera.transform.rotation = editorCamera.transform.rotation;
+                    }
+                }
+            }
+        }
+
+#endif
+    }
+}
